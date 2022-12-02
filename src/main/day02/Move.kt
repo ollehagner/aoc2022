dd5e16be9d2877ffb131c77e01f8113417355674
Olle Hagner <olle.hagner@gmail.com>
2022-12-02 23:17:17 +0100

    Remove unused code

--- src/main/day02/Move.kt
+++ src/main/day02/Move.kt
@@ -10,13 +10,6 @@ enum class Move(val score: Int) {
             }
         }
 
-        override fun moveForResult(result: Result): Move {
-            return when(result) {
-                Result.LOSS -> PAPER;
-                Result.WIN -> SCISSORS
-                Result.DRAW -> ROCK
-            }
-        }
     }, PAPER(2) {
         override fun play(move: Move): Result {
             return when(move) {
@@ -26,13 +19,6 @@ enum class Move(val score: Int) {
             }
         }
 
-        override fun moveForResult(result: Result): Move {
-            return when(result) {
-                Result.LOSS -> SCISSORS;
-                Result.WIN -> ROCK
-                Result.DRAW -> PAPER
-            }
-        }
     }, SCISSORS(3) {
         override fun play(move: Move): Result {
             return when(move) {
@@ -42,15 +28,7 @@ enum class Move(val score: Int) {
             }
         }
 
-        override fun moveForResult(result: Result): Move {
-            return when(result) {
-                Result.LOSS -> ROCK;
-                Result.WIN -> PAPER
-                Result.DRAW -> SCISSORS
-            }
-        }
     };
 
     abstract fun play(move: Move): Result;
-    abstract fun moveForResult(result: Result): Move;
 }
